2a168ef51a9257251fb808efcd6bc77e50f113e0
Xie B.G. <xiepenguin@gmail.com>
2024-05-14 18:14:13 +0800

    xxPandasQuery enhanced

--- xudfs.py
+++ xudfs.py
@@ -516,14 +516,18 @@ def xxLookupMultiple(lookup_value, lookup_array,return_array):
 @xw.arg("data",convert=pd.DataFrame, index=0, ndim=2,doc=": 待查询的数据区，第一行为列名")
 @xw.arg("expr",doc=": 查询表达式，写法参见pandas文档。如：'A > 0 and `B 1` < 0' and C.str.startswith('a') and D in [1,2,3]'")
 @xw.arg("cols",doc=": 返回各列的列名，多个列名用逗号分隔,默认为空返回全部列")
-@xw.ret(index=False, header=True)
-def xxPandasQuery(data, expr, cols=None):
+@xw.arg("headers",doc=": 是否返回列名，默认为TRUE")
+@xw.ret(index=False)
+def xxPandasQuery(data, expr, cols=None, headers=True):
     """pandas.DataFrame.query()的封装。"""
     if cols:
         qry = data.query(expr, inplace=False)[cols.split(",")]
     else:   
         qry = data.query(expr, inplace=False)
-    return qry
+    if headers:
+        return qry
+    else:
+        return qry.values
 
 @xw.func
 @xw.arg("col_index",doc=": 以字母表示的列索引")
